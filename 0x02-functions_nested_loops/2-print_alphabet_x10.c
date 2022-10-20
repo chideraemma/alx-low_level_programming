4e4d3df35d62b5ea81565fddb997db256ceeba3e
chideraemma <emmachidera278@gmail.com>
2022-10-19 20:28:51 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,6 @@
 /**
  * print_alphabet_x10 - print 10 times the alphabet, in lowercase
  */
-
 void print_alphabet_x10(void)
 {
 	int count = 0;
@@ -15,4 +14,5 @@ void print_alphabet_x10(void)
 	for (letter = 'a'; letter <= 'z'; lettter++)
 		_putchar(letter);
 	_putchar('\n');
+	}
 }
