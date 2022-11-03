82c3c066bc840ec7b374549b9378cc3ec6793186
chideraemma <emmachidera278@gmail.com>
2022-11-03 15:32:57 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	int i = 0;
 
 	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		i++;
+		i += _strlen_recursion(s + 1);
+
 	}
+	return (i);
+}
