f33fad1034bc77ed175babc4eab2ab6c8a23aa39
Ubah Emmanuel Chidera <emmachidera278@gmail.com>
2023-02-17 13:09:40 -0800

    C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#incluede <stdio.h>
+
+int main(void)
+{
+	printf("Size of char: %d byte(s)", sizeof(char));
+	printf("Size of int: %d byte(s)", sizeof(int));
+	printf("Size of long int %d byte", sizeof(long int));
+	printf("Size of long long int %d byte", sizeof(long long int);
+	printf("Size of float %d byte", sizeof(float));
+
+	return (0);
+}
