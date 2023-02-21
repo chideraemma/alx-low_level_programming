ca299570243b8cea830d6ea06fc9b0888f33b60a
Ubah Emmanuel Chidera <emmachidera278@gmail.com>
2023-02-20 17:54:33 -0800

    Variables, if, else, while C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
